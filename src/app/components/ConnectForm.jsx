2b3f843b08bfb82009e6d12d63e91f9e8bbec633
jonayed-nasir <junayednasir073@gmail.com>
2026-01-25 20:04:00 +0600

    my app test project v.0.1

---
+++ src/app/components/ConnectForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ConnectForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ConnectForm
---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,17 @@
+// app/dashboard/layout.jsx
+
+
+import Sidebar from "../components/Sidebar";
+import TopNav from "../components/TopNav";
+
+export default function dashboardLayout({children}) {
+    return (
+        <div className="flex h-screen bg-gray-100">
+            <Sidebar />
+            <div className="flex-1 flex flex-col">
+                <TopNav />
+                <main className="p-4 flex-1 overflow-y-auto">{children}</main>
+            </div>
+        </div>
+    )
+}
