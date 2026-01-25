873390d2d68ab7e334c35ed7abcf5ae51634b61b
jonayed-nasir <junayednasir073@gmail.com>
2026-01-25 22:42:04 +0600

    sheet update v1.1.1

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import SheetTabs from "@/components/SheetTabs";
-import Spreadsheet from "@/components/Spreadsheet";
-
+import SheetTabs from "../components/SheetTabs";
+import Spreadsheet from "../components/Spreadsheet";
 
 export default function SheetPage() {
   const [sheets, setSheets] = useState([
