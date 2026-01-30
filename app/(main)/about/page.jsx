3100841220a40e63c7d4a83d3c1c7a4618d083b5
jonayed-nasir <junayednasir073@gmail.com>
2026-01-30 11:54:52 +0600

    forgot-password && reset-password

--- app/(main)/about/page.jsx
+++ app/(main)/about/page.jsx
@@ -1,13 +1,17 @@
 // app/about/page.js
 export default function AboutPage() {
   return (
-    <section className="py-20 px-6 max-w-4xl mx-auto">
+    <section className="py-20 mt-9 px-6 max-w-4xl mx-auto">
       <h2 className="text-3xl font-bold mb-6">About Us</h2>
       <p className="text-gray-700 leading-7 mb-4">
         We are building automation tools for marketers & businesses to grow faster using Facebook.
       </p>
       <p className="text-gray-700 leading-7">
         Our mission is simple: make automation accessible, powerful, and affordable.
+
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, recusandae sed eveniet, minima sunt quos ipsa quo quas similique ratione nihil laboriosam repellat neque nesciunt quam. Numquam nemo asperiores maiores.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti minima ratione sint. Rem eligendi, ullam qui, libero velit doloribus ab doloremque aliquid assumenda nesciunt odit voluptas nulla expedita quo autem iste magni? Expedita obcaecati, ipsam quibusdam vel porro similique quam aliquam consequuntur iste. Inventore nemo commodi corrupti quae, dolor ut totam quo. Odio, minima nemo, consequuntur voluptatum iste laboriosam eius iure aliquid magnam unde tvoluptatem accusantium ea, illum ratione in natus ullam eum iste! Itaque, sint perspiciatis et explicabo repudiandae nesciunt eos deleniti totam rem, perferendis sapiente nam asperiores odit quae veniam dolores dolore ducimus, illum fuga tenetur vitae natus minus aspernatur similique! Odit iste cumque laboriosam molestiae. Quisquam eveniet cum facilis fugiat quos error fugit vel voluptate possimus explicabo iste sunt nam autem quod cupiditate distinctio, aspernatur iusto voluptates asperiores beatae. Reprehenderit iusto aspernatur repellat fugit consequatur in odit perferendis quod quasi exercitationem quibusdam voluptas cum et, optio, provident nostrum asperiores similique numquam, accusamus non rem ex quae! Repellendus ratione voluptas iure fugit aspernatur delectus fuga non voluptate, quisquam magni. Itaque cupiditate quis consectetur nam non dolore totam aliquam laborum rerum, vitae ex illum id voluptatibus magnam amet tenetur fugit minima eaque aut iure voluptates, autem ipsa mollitia? Aut dolorum alias iusto magni ratione id sint veritatis libero.
       </p>
     </section>
   );
